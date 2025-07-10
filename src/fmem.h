a3bddd25aeb6a31b004cdd433b15b8e6a4b87043
Jonathan Woodruff <jonwoodruff@gmail.com>
2025-07-10 16:13:07 +0100

    Remove debug output from fmem.h

--- src/fmem.h
+++ src/fmem.h
@@ -65,8 +65,8 @@ uint32_t fmem_read(int fd, uint32_t offset, uint8_t width)
         uint32_t dat_mask = -1;
         if (width == 1) dat_mask = 0xFF;
         if (width == 2) dat_mask = 0xFFFF;
-        fprintf(stdout, "read! offset: %x, req.data: %x, adr_mask: %x, dat_mask: %x \r\n", offset, req.data, adr_mask, dat_mask);
-        fflush(stdout);
+        //fprintf(stdout, "read! offset: %x, req.data: %x, adr_mask: %x, dat_mask: %x \r\n", offset, req.data, adr_mask, dat_mask);
+        //fflush(stdout);
         return ((wide >> ((offset & ~adr_mask)*8)) & dat_mask);
     } else return (0);
 }
@@ -95,8 +95,8 @@ uint64_t fmem_write(int fd, uint32_t offset, uint32_t data, uint8_t width)
     req.offset = offset;
     req.data = data;
     req.access_width = width;
-    fprintf(stdout, "write! offset: %x, req.data: %x, width: %x \r\n", offset, req.data, width);
-    fflush(stdout);
+    //fprintf(stdout, "write! offset: %x, req.data: %x, width: %x \r\n", offset, req.data, width);
+    //fflush(stdout);
     error = ioctl(fd, FMEM_WRITE, &req);
     return (error);
 }
