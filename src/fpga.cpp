297863665125c97283ff5f8db028a21687384dcd
Jonathan Woodruff <jdw57@cam.ac.uk>
2025-07-14 15:44:05 +0100

    Remove debug output.

--- src/fpga.cpp
+++ src/fpga.cpp
@@ -147,9 +147,9 @@ uint32_t FPGA_io::dma_set_window(uint64_t addr) {
     uint64_t offset = addr & (~DMA_WINDOW_MASK);
     if (offset != last_offset) {
         if (selector_fd >= 0) {
-            printf("writing address selector (write) 0x0 == 0x%" PRIx64 ", addr was 0x%" PRIx64 ", return offset is 0x%" PRIx64 "\n",
-                    offset, addr, (uint32_t)(addr & DMA_WINDOW_MASK));
-            fflush(stdout);
+            //printf("writing address selector (write) 0x0 == 0x%" PRIx64 ", addr was 0x%" PRIx64 ", return offset is 0x%" PRIx64 "\n",
+            //        offset, addr, (uint32_t)(addr & DMA_WINDOW_MASK));
+            //fflush(stdout);
             int error = fmem_write64(selector_fd, 0, offset);
             if (error != 0) {
                 printf("error with address selector (write) 0x0 == 0x%" PRIx64 "\n",
