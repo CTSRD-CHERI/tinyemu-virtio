f188f3cf6910a16ef5b9d3b70cac11dbd1f6104b
Jonathan Woodruff <jdw57@cam.ac.uk>
2025-07-14 14:53:22 +0100

    Ensure the mask is a 64-bit value.

--- src/fpga.cpp
+++ src/fpga.cpp
@@ -130,8 +130,8 @@ void FPGA_io::close_dma()
 }
 */
 
-// A bitmask indicating the width of the DMA window - here, 32 bits
-#define DMA_WINDOW_MASK 0x3FFFFFFFu
+// A bitmask indicating the width of the DMA window - here, 30 bits
+#define DMA_WINDOW_MASK 0x3FFFFFFFull
 // fmem_{read,write} truncate the address to 32-bits (thanks, C++ numeric coercion >:P)
 // so dma_set_window returns a truncated address masked by the window
 // => the DMA window must be at most 32-bits wide.
